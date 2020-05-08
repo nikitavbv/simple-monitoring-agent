c49d5ecd1c70f0c01fd6f3f69adabf796df0a579
Nikita Volobuev <nikitavbv@gmail.com>
2020-05-08 23:51:50 +0300

    feat(io): add metric encoding

--- src/io.rs
+++ src/io.rs
@@ -10,7 +10,7 @@ use async_trait::async_trait;
 
 use crate::database::Database;
 use crate::config::get_max_metrics_age;
-use crate::types::{Metric, MetricCollectionError, MetricSaveError, MetricCleanupError, MetricCollector};
+use crate::types::{Metric, MetricCollectionError, MetricSaveError, MetricCleanupError, MetricCollector, MetricEncodingError};
 use sqlx::{PgConnection, Pool};
 
 #[derive(Debug, Clone)]
@@ -107,6 +107,15 @@ impl MetricCollector for IOMetricCollector {
         }
 
         Ok(())
+    }
+
+    async fn encode(&self) -> Result<String, MetricEncodingError> {
+        if let Some(metric) = &self.metric {
+            let v = serde_json::to_string(metric)?;
+            Ok(v)
+        }
+
+        Err(MetricEncodingError::NoRecord)
     }
 
     async fn cleanup(&self, mut database: &Database) -> Result<(), MetricCleanupError> {
